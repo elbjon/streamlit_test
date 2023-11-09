6da65b46c1def50c18f1f34c6494a54de1ae8f21
Jonas Elbers <143721070+elbjon@users.noreply.github.com>
2023-11-09 14:45:02 +0100

    strl.py

--- strl.py
+++ strl.py
@@ -1,16 +1,19 @@
 import streamlit as st
+import pandas as pd
 
 
 
 
 
 
-url = 'https://drive.google.com/file/d/1hgz5gpLuuZTxSEx9jixkWaTIfjAhkYV8/view?usp=sharing'
-path = 'https://drive.google.com/uc?id='+url.split('/')[-2]
-moviescorrelationsmatrix   = pd.read_csv(path)
+
 
 usermoviesmatrix = pd.read_csv('umm.csv')
 
 df = pd.read_csv('ratings.csv')
 
 dfmovies = pd.read_csv('movies.csv')
+
+url = 'https://drive.google.com/file/d/1hgz5gpLuuZTxSEx9jixkWaTIfjAhkYV8/view?usp=sharing'
+path = 'https://drive.google.com/uc?id='+url.split('/')[-2]
+moviescorrelationsmatrix = pd.read_csv(path)
