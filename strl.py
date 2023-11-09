f704f710e4340b36815c094fc176570a93acf115
Jonas Elbers <143721070+elbjon@users.noreply.github.com>
2023-11-09 14:54:11 +0100

    strl.py

--- strl.py
+++ strl.py
@@ -5,15 +5,20 @@ import pandas as pd
 
 
 
+url = 'https://drive.google.com/file/d/1hgz5gpLuuZTxSEx9jixkWaTIfjAhkYV8/view?usp=sharing'
+path = 'https://drive.google.com/uc?id='+url.split('/')[-2]
+mcm = pd.read_csv(path)
 
+user_movies_matrix = pd.pivot_table(data=mcm,
+                                  values='rating',
+                                  index='userId',
+                                  columns='movieId',
+                                  fill_value=0)
 
-
-usermoviesmatrix = pd.read_csv('umm.csv')
+user_movies_matrix = pd.read_csv('umm.csv')
 
 df = pd.read_csv('ratings.csv')
 
-dfmovies = pd.read_csv('movies.csv')
+df_movies = pd.read_csv('movies.csv')
 
-url = 'https://drive.google.com/file/d/1hgz5gpLuuZTxSEx9jixkWaTIfjAhkYV8/view?usp=sharing'
-path = 'https://drive.google.com/uc?id='+url.split('/')[-2]
-moviescorrelationsmatrix = pd.read_csv(path)
+st.write('end of program')
