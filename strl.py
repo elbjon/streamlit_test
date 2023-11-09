9aaf547b185ceaa560f672ba4a494364b129a085
Jonas Elbers <143721070+elbjon@users.noreply.github.com>
2023-11-09 15:05:03 +0100

    strl.py

--- strl.py
+++ strl.py
@@ -4,13 +4,9 @@ import pandas as pd
 
 url = 'https://drive.google.com/file/d/1hgz5gpLuuZTxSEx9jixkWaTIfjAhkYV8/view?usp=sharing'
 path = 'https://drive.google.com/uc?id='+url.split('/')[-2]
-user_movies_matrix = pd.read_csv(path)
+movies_cosines_matrix = pd.read_csv(path)
 
-#user_movies_matrix = pd.pivot_table(data=mcm,
-#                                  values='rating',
-#                                  index='userId',
-#                                  columns='movieId',
-#                                  fill_value=0)
+
 st.write(user_movies_matrix.shape)
 st.write(user_movies_matrix.info())
 st.write(user_movies_matrix.describe())
