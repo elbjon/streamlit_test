fd3c4c796b508e67e9f50e46fdd1b02534e5ea68
Jonas Elbers <143721070+elbjon@users.noreply.github.com>
2023-11-09 14:56:45 +0100

    Update strl.py

--- strl.py
+++ strl.py
@@ -2,18 +2,17 @@ import streamlit as st
 import pandas as pd
 
 
-
-
-
 url = 'https://drive.google.com/file/d/1hgz5gpLuuZTxSEx9jixkWaTIfjAhkYV8/view?usp=sharing'
 path = 'https://drive.google.com/uc?id='+url.split('/')[-2]
-mcm = pd.read_csv(path)
+user_movies_matrix = pd.read_csv(path)
 
-user_movies_matrix = pd.pivot_table(data=mcm,
-                                  values='rating',
-                                  index='userId',
-                                  columns='movieId',
-                                  fill_value=0)
+#user_movies_matrix = pd.pivot_table(data=mcm,
+#                                  values='rating',
+#                                  index='userId',
+#                                  columns='movieId',
+#                                  fill_value=0)
+
+st.write(user_movie_matrix[2])
 
 user_movies_matrix = pd.read_csv('umm.csv')
 
